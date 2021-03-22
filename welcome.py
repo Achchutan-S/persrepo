161d8f2725c0501749b64b386e8e86b62c7c48c8
Achchutan S <achchutan.srinivasan@gmail.com>
2021-03-22 13:34:01 +0530

    welcome is modified

--- welcome.py
+++ welcome.py
@@ -1,2 +1,12 @@
 for i in range(10):
     print("Welcome to git ")
+print("Done with for loops")
+a=10
+print(a)
+b=[2,"Achu",2.34]
+i=0
+while (b!=0):
+    print(b[i])
+    i=i+1
+    if i==3:
+        break
